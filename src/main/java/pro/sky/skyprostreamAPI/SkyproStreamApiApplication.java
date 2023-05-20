79f494f909dca00e42f63b3b2cfdc0a65065725b
Екатерина <kate.frolova.16@gmail.com>
2023-05-20 19:45:41 +0300

    first commit

---
+++ src/main/java/pro/sky/skyprostreamAPI/SkyproStreamApiApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprostreamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproStreamApiApplication.class, args);
+	}
+
+}
